e2125bb801c96a15821525922bb35d800b5b7192
Maxdev01 <61092309+Maxdev01@users.noreply.github.com>
2021-03-27 19:11:59 -0700

    deploye

--- dorsforma/settings.py
+++ dorsforma/settings.py
@@ -123,7 +123,29 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   'dorsforma/static',
+   os.path.join(BASE_DIR, 'static')
 
 ]
+
+import django_heroku
+import dj_database_url
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'idel',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+} 
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)  
+
+
+
+django_heroku.settings(locals())
